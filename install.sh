a98f683be5eb9c11a9ac94f354e2d492953121d4
gitclone93 <65633360+gitclone93@users.noreply.github.com>
2020-08-28 12:54:39 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@ echo Installing...
 echo Please Enter Password when prompted.
 sudo mkdir /Applications/KernelPanicv1/
 sudo echo "#!/bin/bash" >> /Applications/KernelPanicv1/crash.sh
-sudo echo sudo dtrace -w -n "BEGIN{panic();}" >> /Applications/KernelPanicv1/crash.sh
+sudo echo sudo dtrace -w -n "BEGIN{ panic();}" >> /Applications/KernelPanicv1/crash.sh
 echo Installed.
